4d0823f9aed7ea1e0018ebac9ecb8a08fa086159
R B <rons9n@gmail.com>
2021-01-05 20:27:55 -0500

    defined categories

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   TextField,
   Typography,
@@ -12,6 +12,13 @@ import {
 
 import useStyles from './styles';
 
+const initialState = {
+  amount: '',
+  category: '',
+  type: 'Income',
+  date: new Date()
+}
+
 const Form = () => {
   const classes = useStyles();
   return (
@@ -24,7 +31,7 @@ const Form = () => {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Type</InputLabel>
-          <Select>
+          <Select value={FormData.type} onChange={(e) => setFormData({ ...FormData, type: e.target.value })}>
             <MenuItem value='Income'>Income</MenuItem>
             <MenuItem value='Expense'>Expense</MenuItem>
           </Select>
@@ -33,17 +40,17 @@ const Form = () => {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Category</InputLabel>
-          <Select>
+          <Select value={FormData.category} onChange={(e) => setFormData({ ...formData, category: e.target.value })}>
             <MenuItem value='business'>Business</MenuItem>
             <MenuItem value='salary'>Salary</MenuItem>
           </Select>
         </FormControl>
       </Grid>
       <Grid item xs={6}>
-        <TextField type='number' label='Amount' fullWidth />
+        <TextField type='number' label='Amount' fullWidth value={formData.amount} onChange={(e) => setFormData({ ... formData, amount: e.target.value })} />
       </Grid>
       <Grid item xs={6}>
-        <TextField type='date' label='Date' fullWidth />
+        <TextField type='date' label='Date' fullWidth value={formData.date} onChange={(e) => setFormData({ ... formData, date: e.target.value })} /> />
       </Grid>
       <Button
         className={classes.button}
