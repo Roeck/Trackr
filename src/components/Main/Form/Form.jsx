a5ad02f50c171097b6ff3961e0981610c0d5a7b3
R B <rons9n@gmail.com>
2021-01-16 22:13:36 -0500

    InfoCard component

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -31,6 +31,7 @@ const Form = () => {
   const { segment } = useSpeechContext();
 
   const createTransaction = () => {
+    if(Number.isNaN(Number(formData.amount)) || !formData.date.includes('-')) return;
     const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4() }
 
     addTransaction(transaction);
@@ -50,13 +51,18 @@ const Form = () => {
       }
 
       segment.entities.forEach((e) => {
-        const category = `${e.value.chartAt(0)}${s.value.slice(1).toLowerCase()}`;
+        const category = `${e.value.charAt(0)}${e.value.slice(1).toLowerCase()}`;
         switch (e.type) {
           case 'amount':
             setFormData({ ...formData, amount: e.value });
           break;
           case 'category':
-            setFormData({ ...formData, category: e.value });
+            if(incomeCategories.map((iC) => iC.type).includes(category)) {
+              setFormData({ ...formData, type: 'Income', category });
+            } else if (expenseCategories.map((iC) => iC.type).includes(category)) {
+              setFormData({ ...formData, type: 'Expense', category });
+            }
+            setFormData({ ...formData, category });
           break;
           case 'date':
             setFormData({ ...formData, date: e.value });
@@ -65,6 +71,9 @@ const Form = () => {
             break;
         }
       });
+
+      if(segment.isFinal && formData.amount && formData.category && formData.type && formData.date)
+        createTransaction();
     }
   }, [segment]);
 
